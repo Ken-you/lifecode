d7855b6b9bcebd8c276a25b1f0c3dc12129a9edd
yousun <a0921990065@gmail.com>
2021-05-08 16:24:57 +0800

    Initial Commit

---
+++ lifecode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lifecode
+//
+//  Created by yousun on 2021/5/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
